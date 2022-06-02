566f48c5d266969031d9df9fcae02e4cb2087181
Jean Fontena <diegofontena@gmail.com>
2022-06-02 16:58:52 +0200

    Create energy_bencher structure (but it doesn't do anything yet)

---
+++ src/energy_bencher.rs
@@ -0,0 +1,19 @@
+use crate::bencher::Bencher;
+
+pub struct EnergyBencher {}
+
+impl EnergyBencher {
+    pub fn new() -> Self {
+        unimplemented!()
+    }
+}
+
+impl Bencher for EnergyBencher {
+    fn start(&mut self) {
+        unimplemented!();
+    }
+
+    fn end(&mut self) {
+        unimplemented!();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 mod bencher;
-pub use bencher::BenchSuite;
+pub use bencher::{BenchSuite, Bencher};
 
 mod cpu_time_bencher;
 pub use cpu_time_bencher::CpuTimeBencher;
+
+mod energy_bencher;
+pub use energy_bencher::EnergyBencher;
