98fa86a4cd6eaac9f15eb8125ec08e6b2d3a8a7f
Jean Fontena <diegofontena@gmail.com>
2022-06-02 10:36:15 +0200

    Turn it into a lib and add a BenchResult structure

--- src/bencher.rs
+++ src/bencher.rs
@@ -1,15 +1,30 @@
 use cpu_time::ProcessTime;
 use std::time::Duration;
 
+#[derive(Debug)]
 pub struct Bencher {}
 
 impl Bencher {
-    pub fn bench<F>(function: F) -> Duration
+    pub fn bench<F>(function: F) -> BenchResult
     where
         F: Fn(),
     {
         let start = ProcessTime::now();
         function();
-        start.elapsed()
+
+        BenchResult {
+            cpu_time: start.elapsed(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct BenchResult {
+    cpu_time: Duration,
+}
+
+impl BenchResult {
+    pub fn cpu_time(&self) -> Duration {
+        self.cpu_time
     }
 }
---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+mod bencher;
+pub use bencher::{BenchResult, Bencher};
