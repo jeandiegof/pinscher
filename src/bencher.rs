5a8705e8310e436a6b83da6b75a519e5753c9c79
Jean Fontena <diegofontena@gmail.com>
2022-06-02 16:21:31 +0200

    Add some thoughts on how the implementation should proceed and some tests using powercap interface

--- src/bencher.rs
+++ src/bencher.rs
@@ -1,5 +1,50 @@
 use cpu_time::ProcessTime;
+use powercap::PowerCap;
 use std::time::Duration;
+
+fn energy_begin() {
+    let intel_rapl = PowerCap::try_default().unwrap().intel_rapl;
+
+    // this is supposed to be intel-rapl/intel-rapl:0, which is "package"
+    // cat /sys/class/powercap/intel-rapl/intel-rapl:0/name
+    let package = intel_rapl.sockets.get(&0).unwrap();
+    let package_energy = package.energy().unwrap();
+
+    // this is supposed to be /sys/class/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:0
+    // cat /sys/class/powercap/intel-rapl/intel-rapl:0/intel-rapl:0:0/name
+    let core = package.domains.get(&0).unwrap();
+    let core_energy = core.energy().unwrap();
+
+    // println!("{:#?}", powercap.intel_rapl.snapshot());
+    println!("package: {:?} core: {:?}", package_energy, core_energy);
+    assert!(package_energy > core_energy);
+
+    // TODO: which values are we interested in? How could we generalize it for other processors?
+    // Mine has only two the package and the core, but others may have more sensors.
+    // How to customize the bench so that we can use custom benchers that return different types of BenchResult?
+    // If we have a CpuTimeBencher and a EnergyBencher, we could have CpuTimeBenchResult and EnergyBenchResult, but that
+    // wouldn't scale well.
+    // At the end, what matters for us to be able to serialize a BenchResult. If that's true, Bencher::bench could return
+    // something that implements a serializable trait.
+}
+
+fn energy_end() {
+    let powercap = PowerCap::try_default().unwrap();
+    println!("Total energy: {:?} uJ", powercap.intel_rapl.total_energy());
+    // println!("{:#?}", powercap);
+}
+
+// a bencher trait? fn start, fn end
+// the BenchSuite (currently named Bencher but that has to change)
+// could store a object implementing the trait Bencher (ex: CpuTimeBencher, EnergyBencher, etc)
+// and call start and stop of those methods.
+// The open question is: how would the user get the output? Should the bench suite return it? should the user
+// call .results() in the CpuTimeBencher (for example) and then receive a output struct specific for this type?
+// That looks reasonable. Cons: initially I thought it would be hard to write the outputs of different benchers
+// to a file, but we could implement Serializable for the custom benchs and that should work
+// Another option: parametrize the type of the bencher. The bench function would become bench<F, A> where A is the type of the custom bencher.
+// which has to implement the trait. The advantage is that we could return A as well in bench. So it would become bench<F, A>(function: F, bencher: A) -> A,
+// where A: impl Bencher
 
 #[derive(Debug)]
 pub struct Bencher {}
@@ -9,8 +54,10 @@ impl Bencher {
     where
         F: Fn(),
     {
+        energy_begin();
         let start = ProcessTime::now();
         function();
+        energy_end();
 
         BenchResult {
             cpu_time: start.elapsed(),
